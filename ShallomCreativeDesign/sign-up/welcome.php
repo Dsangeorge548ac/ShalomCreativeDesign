77edbce52a4b21e63beafa87448efb83ebd1153f
Dsangeorge548ac <140263069+Dsangeorge548ac@users.noreply.github.com>
2023-11-15 15:57:02 -0400

    welcome.php

--- ShallomCreativeDesign/sign-up/welcome.php
+++ ShallomCreativeDesign/sign-up/welcome.php
@@ -1,64 +1,337 @@
 <?php
-   session_start();
-   if (!isset($_SESSION['SESSION_EMAIL'])) {
-       header("Location: index.php");
-       die();
-   }
-   
-   include 'config.php';
-   
-   $query = mysqli_query($conn, "SELECT * FROM users WHERE email='{$_SESSION['SESSION_EMAIL']}'");
-   
-   if (mysqli_num_rows($query) > 0) {
-       $row = mysqli_fetch_assoc($query);
-       $name = $row['name'];
-       $email = $row['email'];
-   }
+session_start();
+if (!isset($_SESSION['SESSION_EMAIL'])) {
+    header("Location: index.php");
+    die();
+}
+
+include 'config.php';
+
+if (isset($_POST['upload_image'])) {
+    $name = $_SESSION['SESSION_EMAIL'];
+    $image = $_FILES['profile_image']['tmp_name'];
+    $image_data = file_get_contents($image);
+
+    $update_query = "UPDATE users SET profile_image = ? WHERE email = ?";
+    $stmt = $conn->prepare($update_query);
+    $stmt->bind_param("bs", $image_data, $name);
+
+    if ($stmt->execute()) {
+        echo "Imagen de perfil guardada correctamente.";
+    } else {
+        echo "Error al guardar la imagen de perfil.";
+    }
+}
+
+$query = mysqli_query($conn, "SELECT * FROM users WHERE email='{$_SESSION['SESSION_EMAIL']}'");
+
+if (mysqli_num_rows($query) > 0) {
+    $row = mysqli_fetch_assoc($query);
+    $name = $row['name'];
+    $email = $row['email'];
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/main.css">
+    <title>Perfil</title>
+    <link rel="stylesheet" type="text/css" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 </head>
 <body>
-<header id="header">
-        <h1><img src="../imagenes/fotos/3-removebg-preview.png" alt="" class="logo"></h1>
-        <input type="checkbox" id="nav_check" hidden>
-        <nav>
-            <ul class="menu-horizontal">
-                <li><a href="../index.php" class="link">Inicio</a></li>
-                <li><a href="" class="link">Diseños</a></li>
-                <li><a href="" class="link">Diseñar</a></li>
-                <li><a href="#NOSOTROS" class="link">Nosotros</a></li>
-                <li><a href="" class="link">Support</a></li>
 
-                <ul class="menu-horizontal-2">
-                    <li><a href="#" class="link"><img src="../imagenes/fotos/usuario.png" alt="" class="icon"></a></li>
-                    <li><a href="#" class="link-2">Registrarse</a></li>
-                </ul>
-            </ul>
-        </nav>
-        
-        <label for="nav_check" class="hamburger">
-            <div></div>
-            <div></div>
-            <div></div>
-        </label>
-    </header>
+<style>
 
-<div class="data-container">
-    <form action="welcome.php" method="post" class="data">
-            <div class="profile-container">
-                <img src="./images/usuario.png" id="image">
-                <h2 id="name"><?php echo $name; ?></h2>
-                <p id="email"><?php echo $email; ?></p>
-                <a href='logout.php' id="row" class="logout">Sign out</a>
+html {
+    -webkit-text-size-adjust: 100%;
+    -ms-text-size-adjust: 100%;
+    text-size-adjust: 100%;
+    line-height: 1.4;
+}
+
+
+* {
+    margin: 0;
+    padding: 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+
+body {
+    color: #404040;
+    font-family: "Arial", Segoe UI, Tahoma, sans-serifl, Helvetica Neue, Helvetica;
+}
+
+.perfil-usuario {
+    background: linear-gradient(#bababa, transparent);
+    color: #333;
+}
+.perfil-usuario .sombra {
+    position: absolute;
+    display: block;
+    background: linear-gradient(transparent,rgba(0, 0, 0, .5));
+    width: 100%;
+    height: 50%;
+    bottom: 0;
+    left: 0;
+}
+.perfil-usuario .portada-perfil,
+.perfil-usuario .sombra {
+    border-radius: 0 0 20px 20px;
+}
+.perfil-usuario img {
+    width: 100%;
+}
+.contenedor-perfil {
+    max-width: 1024px;
+    margin-left: auto;
+    margin-right: auto;
+}
+.perfil-usuario .contenedor-perfil {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 90%;
+}
+.perfil-usuario .portada-perfil {
+    display: block;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    position: relative;
+    height: 20rem;
+    margin-bottom: .5rem;
+}
+.perfil-usuario .avatar-perfil {
+    display: block;
+    width: 230px;
+    height: 230px;
+    background-color: #D9DCF1;
+    position: absolute;
+    bottom: -65px;
+    left: 4rem;
+    border-radius: 50%;
+    overflow: hidden;
+    border: 8px solid #FFFFFF;
+    box-shadow: 0 0 12px 2px rgba(0, 0, 0, .2);
+}
+.perfil-usuario .cambiar-foto {
+    position: absolute;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: rgba(0, 0, 0, .5);
+    height: 57%;
+    bottom: 0;
+    left: 0;
+    color: #fff;
+    text-decoration: none;
+    transform: translateY(50%);
+    opacity: 0;
+    transition: all ease-in 200ms;
+}
+.perfil-usuario .cambiar-foto i {
+    margin-bottom: .5rem;
+    font-size: 2rem;
+}
+.perfil-usuario .avatar-perfil:hover .cambiar-foto {
+    transform: translateY(0);
+    opacity: 1;
+}
+.perfil-usuario .datos-perfil {
+    position: absolute;
+    display: block;
+    width: calc(100% - 230px - 8rem);
+    right: 0;
+    bottom: 0;
+    color: #fff;
+    text-shadow: 0 0 5px rgba(0, 0, 0, .2);
+    padding-bottom: 1rem;
+    padding-right: 1rem;
+}
+.perfil-usuario .titulo-usuario {
+    font-size: 2rem;
+    white-space: nowrap;
+    margin-bottom: .5rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+.perfil-usuario .bio-usuario {
+    font-size: 1em;
+    margin-bottom: .75rem;
+}
+.perfil-usuario .lista-perfil {
+    list-style: none;
+}
+.perfil-usuario .lista-perfil li {
+    display: inline-block;
+    font-size: 1em;
+    margin-right: 1rem;
+}
+
+.perfil-usuario .opcciones-perfil {
+    display: block;
+    position: absolute;
+    right: 2rem;
+    top: 1rem;
+}
+.perfil-usuario .opcciones-perfil button {
+    border: 0;
+    padding: 8px 20px;
+    border-radius: 8px;
+    background-color: rgba(0, 0, 0, .5);
+    color: #fff;
+    cursor: pointer;
+}
+.perfil-usuario .menu-perfil ul {
+    display: flex;
+    list-style: none;
+    margin-left: calc(200px + 8rem);
+    width: calc(100% - 200px - 8rem);
+}
+.perfil-usuario .menu-perfil ul li {
+    margin-right: 1rem;
+}
+.perfil-usuario .menu-perfil a {
+    display: block;
+    text-decoration: none;
+    color: inherit;
+    padding: 8px 20px;
+    font-weight: bold;
+    border-radius: 8px;
+    transition: all ease-in 100ms;
+}
+.perfil-usuario .menu-perfil a:hover {
+    background-color: #4CB0C6;
+    color: #fff;
+}
+.perfil-usuario .icono-perfil {
+    display: none;
+    margin-right: .75rem;
+}
+@media (max-width: 780px) {
+    .perfil-usuario .contenedor-perfil {
+        width: 100%;
+    }
+    .perfil-usuario .avatar-perfil {
+        left: calc(50% - 115px)
+    }
+    .perfil-usuario .datos-perfil {
+        bottom: 200px;
+        left: 0;
+        width: 100%;
+        padding: 15px;
+        text-align: center;
+    }
+    .perfil-usuario .bio-usuario {
+        font-size: 1em;
+        overflow:hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+    .perfil-usuario .titulo-usuario {
+        font-size: 2.1rem;
+    }
+    .perfil-usuario .portada-perfil {
+        height: 28rem;
+    }
+    .perfil-usuario .menu-perfil ul {
+        flex-direction: column;
+    }
+    .perfil-usuario .lista-perfil {
+        display: block;
+    }
+    .perfil-usuario .menu-perfil {
+        margin-top: 2rem;
+    }
+    .perfil-usuario .menu-perfil ul {
+        display: flex;
+        list-style: none;
+        margin-left: auto;
+        margin-right: auto;
+        padding-top: 2.5rem;
+        width: 70%;
+        background-color: #fff;
+        border-radius: 12px;
+        box-shadow: 0 0 12px 2px rgba(0, 0, 0, .1);
+    }
+    .perfil-usuario .icono-perfil {
+        display: inline-block;
+    }
+}
+</style>
+<section class="perfil-usuario">
+    <div class="contenedor-perfil">
+        <div class="portada-perfil" style="background-image: url('http://localhost/multimedia/relleno/fondo-9.png');">
+            <div class="sombra"></div>
+            <div class="avatar-perfil">
+                <img src="../imagenes/fotos/usuario.png" alt="Foto">
+                <label class="cambiar-foto">
+                    <input type="file" accept="image/*" id="file-input" style="display: none;">
+                    <i class="fas fa-camera"></i> 
+                    <span>Cambiar foto</span>
+                </label>
             </div>
-        </form>
+            <div class="datos-perfil">
+                <h3 id="name" class="titulo-usuario"><?php echo $name; ?></h2>
+                <p id="email" class="bio-usuario"><?php echo $email; ?></p>
+            </div>
+            <div class="opcciones-perfil">
+                <button onclick="redireccion()">Volver</button>
+                <button type="">Cambiar portada</button>
+            </div>
+        </div>
+        <div class="menu-perfil">
+            <ul>
+                <li><a href="#" title=""><i class="icono-perfil fas fa-bullhorn"></i> Publicaciones</a></li>
+                <li><a href="#" title=""><i class="icono-perfil fas fa-info-circle"></i> Informacion</a></li>
+            </ul>
+        </div>
     </div>
+</section>
+
+
+
+<script>
+
+    // Escuchar el evento de cambio en el input de archivo
+    document.querySelector("#file-input").addEventListener("change", function (e) {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+    
+            // Cuando la imagen se carga correctamente
+            reader.onload = function (e) {
+                const imageSrc = e.target.result;
+                document.querySelector(".avatar-perfil img").src = imageSrc;
+    
+                // Guardar la imagen en el almacenamiento local
+                localStorage.setItem('profileImage', imageSrc);
+            };
+    
+            // Leer la imagen como una URL de datos
+            reader.readAsDataURL(file);
+        }
+    });
+    
+    // Verificar si hay una imagen en el almacenamiento local
+    const storedImage = localStorage.getItem('profileImage');
+    
+    if (storedImage) {
+        document.querySelector(".avatar-perfil img").src = storedImage;
+    }
+
+    function redireccion () {
+        location.href="../index.php";
+    }
+    </script>
 </body>
+
 </html>
